33ee4f89f26130eb39e69c5554ad6fa74c89ffb3
Martin Vicknair <martinvicknair@gmail.com>
2019-04-01 09:58:26 -0600

    gh-pages deployment

---
+++ build/precache-manifest.dc4c31ddac8cae0a10be48e303c0e4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/folio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0209e86f1d7a5ed2c19",
+    "url": "/folio/static/js/runtime~main.e9870f6d.js"
+  },
+  {
+    "revision": "66c80646cf312f6fa7f0",
+    "url": "/folio/static/js/main.0ea68d8e.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/folio/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "66c80646cf312f6fa7f0",
+    "url": "/folio/static/css/main.1beae849.chunk.css"
+  },
+  {
+    "revision": "b9bf43c601e7c905b1c66f5d8d06b00e",
+    "url": "/folio/index.html"
+  }
+];
